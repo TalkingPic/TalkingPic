f46e3aa870347bd3542b0837213b844c01f654e7
Haodan Huang <hah2128@columbia.edu>
2012-10-23 16:25:27 -0400

    master branch

---
+++ TalkingPic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TalkingPic
+//
+//  Created by 黄 昊丹 on 12-10-23.
+//  Copyright (c) 2012年 Howard Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TalkingPic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TalkingPic
+//
+//  Created by 黄 昊丹 on 12-10-23.
+//  Copyright (c) 2012年 Howard Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
